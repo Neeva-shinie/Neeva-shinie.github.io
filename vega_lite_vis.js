b5e674983ca3fe49467441c7eee309e378f98dfc
npar0015 <npar0015@student.monash.edu>
2023-10-07 15:36:09 +0800

    added new visualisation

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,5 +1,11 @@
 var vg_1 = "map.json";
 var vg_2 = "bubbleplot.json";
-vegaEmbed("#choropleth_map", vg_1),("#bar_chart", vg_2).then(function(result) {
+vegaEmbed("#choropleth_map", vg_1).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
+
+vegaEmbed("#bar_chart", vg_2).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+
