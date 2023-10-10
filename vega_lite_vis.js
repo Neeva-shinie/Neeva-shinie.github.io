e17095d84e857620698e00d3d9af0035f4a064fc
npar0015 <npar0015@student.monash.edu>
2023-10-11 04:55:54 +0800

    added all visualisation

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,10 +1,20 @@
 var vg_1 = "map.json";
 var vg_2 = "barchart.json";
+var vg_3 = "donutchart.json";
+var vg_4 = "heatmap.json";
 vegaEmbed("#choropleth_map", vg_1).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 vegaEmbed("#bar_chart", vg_2).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#donutchart", vg_3).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#heatmap", vg_4).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
