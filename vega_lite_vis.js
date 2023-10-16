a17a49418d24bcacb58d5ea16e5d9141cfc4b654
npar0015 <npar0015@student.monash.edu>
2023-10-16 11:59:08 +0800

    edited css file

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,7 +1,9 @@
-var vg_1 = "map.json";
+var vg_1 = "cholopeth.json";
 var vg_2 = "barchart.json";
-var vg_3 = "donutchart.json";
-var vg_4 = "heatmap.json";
+var vg_3 = "heatmap.json";
+var vg_4 = "GMdonut.json";
+var vg_5 = "FMdonut.json";
+var vg_6 = "IMdonut.json";
 vegaEmbed("#choropleth_map", vg_1).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
@@ -10,11 +12,19 @@ vegaEmbed("#bar_chart", vg_2).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-vegaEmbed("#donutchart", vg_3).then(function(result) {
+vegaEmbed("#heatmap", vg_3).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-vegaEmbed("#heatmap", vg_4).then(function(result) {
+vegaEmbed("#GMdonutchart", vg_4).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#FMdonutchart", vg_5).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#IMdonutchart", vg_6).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
